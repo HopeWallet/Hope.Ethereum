7fe7cf5cd186d742e00a485546462082e19eb395
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-26 14:48:35 -0400

    Updating repository name

---
+++ Hope.Ethereum/Hope.Ethereum.Unity/src/Contracts/Abstract/EthereumContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NethereumUtils.Contracts
+{
+    public abstract class EthereumContract
+    {
+        protected EthereumContract(string mainnetAddress)
+        {
+
+        }
+
+        protected EthereumContract(string mainnetAddress, string rinkebyAddress)
+        {
+
+        }
+    }
+}
