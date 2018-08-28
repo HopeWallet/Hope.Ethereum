16fe5e913d0f6a03f8cfc07798b7b3b3fe8067ab
Matthew LeLiever <matthew121250@hotmail.com>
2018-08-27 23:51:42 -0400

    Added Hope.Ethereum.Unity.Tests

---
+++ Hope.Ethereum/Hope.Ethereum.Unity.Tests/Assets/Test.cs
@@ -0,0 +1,35 @@
+﻿using Hope.Ethereum.Unity.Tokens;
+using Hope.Ethereum.Unity.Utils;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private void Start()
+    {
+        NetworkProvider.SwitchNetworkChain(Nethereum.Signer.Chain.MainNet);
+
+        ERC20Tests();
+        ERC721Tests();
+    }
+
+    private static void ERC721Tests()
+    {
+        // NOTE:
+        // Since CryptoKitties was the first draft of ERC721, many functions were not formalized and decided upon.
+        // Therefore, some functions will not work and some others will.
+        ERC721 cryptokitties = new ERC721("0x06012c8cf97BEaD5deAe237070F9587f8E7A266d", "CryptoKitties", "CK", 0);
+
+        cryptokitties.QueryBalanceOf("0x12b353D1a2842D2272aB5A18C6814D69f4296873")
+                     .OnSuccess(balance => Debug.Log(SolidityUtils.ConvertFromUInt(balance.Value, cryptokitties.Decimals.Value)))
+                     .OnError(Debug.Log);
+    }
+
+    private static void ERC20Tests()
+    {
+        ERC20 prps = new ERC20("0xd94F2778e2B3913C53637Ae60647598bE588c570", "Purpose", "PRPS", 18);
+
+        prps.QueryBalanceOf("0xa8EF8e0855F84F25666Cc5b37C5aB8cBF9de314F")
+            .OnSuccess(balance => Debug.Log(SolidityUtils.ConvertFromUInt(balance.Value, prps.Decimals.Value)))
+            .OnError(Debug.Log);
+    }
+}
